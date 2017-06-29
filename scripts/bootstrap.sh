73dbd203f6a6b9f4f4c68ae00d2ed222daae6046
Yanglin Zhao <yz1876@gmail.com>
2017-06-28 22:45:19 -0400

    Add tmux dotfiles

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,6 +4,7 @@ cd dotfiles
 stow -t $HOME bash
 stow -t $HOME fish
 stow -t $HOME git
+stow -t $HOME tmux
 stow -t $HOME vim 
 stow -t $HOME zsh 
 cd -
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -4,6 +4,7 @@ cd dotfiles
 stow -t $HOME -D bash
 stow -t $HOME -D fish
 stow -t $HOME -D git
+stow -t $HOME -D tmux 
 stow -t $HOME -D vim 
 stow -t $HOME -D zsh 
 cd -
