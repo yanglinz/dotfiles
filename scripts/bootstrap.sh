afc9d89dd17a1ff72080beb5edcbc16db9e17671
Yanglin Zhao <yz1876@gmail.com>
2017-06-28 22:00:24 -0400

    Add git dotfiles

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,6 +3,7 @@
 cd dotfiles
 stow -t $HOME bash
 stow -t $HOME fish
+stow -t $HOME git
 stow -t $HOME vim 
 stow -t $HOME zsh 
 cd -
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -3,6 +3,7 @@
 cd dotfiles
 stow -t $HOME -D bash
 stow -t $HOME -D fish
+stow -t $HOME -D git
 stow -t $HOME -D vim 
 stow -t $HOME -D zsh 
 cd -
