5186714bcd292c960cfe238c6e2050e337adc63a
Yanglin Zhao <yz1876@gmail.com>
2018-04-30 14:54:37 -0400

    Manage nvm with fisher instead

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -2,8 +2,3 @@
 # https://github.com/oh-my-fish/theme-bobthefish
 set -g theme_powerline_fonts no
 set -g theme_nerd_fonts no
-
-# nvm (brew)
-# https://github.com/creationix/nvm
-export NVM_DIR="$HOME/.nvm"
-. "/usr/local/opt/nvm/nvm.sh"
