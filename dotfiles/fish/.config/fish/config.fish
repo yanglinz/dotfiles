c925220bb5771d792234a3219db1e6c203b1eb9c
Yanglin Zhao <yz1876@gmail.com>
2018-10-22 11:07:38 -0400

    Add fisher package manager

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -1,3 +1,10 @@
+# Install fisher
+if not functions -q fisher
+  set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+  curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+  fish -c fisher
+end
+
 # Basic themes
 # https://github.com/oh-my-fish/theme-bobthefish
 set -g theme_powerline_fonts no
