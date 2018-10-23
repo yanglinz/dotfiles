869841e304ee3734a3d93ff447e82eadb396c0de
Yanglin Zhao <yz1876@gmail.com>
2018-10-22 23:57:15 -0400

    Link vscode settings

--- scripts/dot.sh
+++ scripts/dot.sh
@@ -8,44 +8,52 @@ red='\033[0;31m'
 active_dotfiles=(bash fish git tmux vim zsh)
 
 function init() {
-    # Initialize git submodule
-    git submodule init
+  # Initialize git submodule
+  git submodule init
 
-    # Setup homebrew
-    brew tap homebrew/bundle
-    brew bundle
+  # Setup homebrew
+  brew tap homebrew/bundle
+  brew bundle
 
-    # Install fonts
-    ./vendor/fonts/install.sh
+  # Install fonts
+  ./vendor/fonts/install.sh
 }
 
 function link() {
-    echo "Linking dotfiles..."
+  echo "Linking dotfiles..."
 
-    cd dotfiles
-    for d in "${active_dotfiles[@]}"; do
-        stow -t $HOME "${d}" && echo -e "${green}Linked ${d}"
-    done
-    cd -
+  cd dotfiles
+  for d in "${active_dotfiles[@]}"; do
+    stow -t $HOME "${d}" && echo -e "${green}Linked ${d}"
+  done
+  cd -
+
+  if [ -x "$(command -v code)" ]; then
+    stow -t "/Users/$(whoami)/Library/Application Support/Code/User" vscode
+  fi
 }
 
 function unlink() {
-    echo "Unlinking dotfiles"
+  echo "Unlinking dotfiles"
 
-    cd dotfiles
-    for d in "${active_dotfiles[@]}"; do
-        stow -t $HOME -D "${d}" && echo -e "${red}Unlinked ${d}"
-    done
-    cd -
+  cd dotfiles
+  for d in "${active_dotfiles[@]}"; do
+    stow -t $HOME -D "${d}" && echo -e "${red}Unlinked ${d}"
+  done
+  cd -
+
+  if [ -x "$(command -v code)" ]; then
+    stow -t "/Users/$(whoami)/Library/Application Support/Code/User" -D vscode
+  fi
 }
 
 function default() {
-    echo "Welcome to the dotfiles script!"
+  echo "Welcome to the dotfiles script!"
 }
 
 case "$1" in
-    init)   init ;;
-    link)   link ;;
-    unlink) unlink ;;
-    *)      default ;;
+  init)   init ;;
+  link)   link ;;
+  unlink) unlink ;;
+  *)      default ;;
 esac
