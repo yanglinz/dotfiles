0df1a52d7d9155f2d5c29fa16fa15e59d800c0de
Yanglin Zhao <yz1876@gmail.com>
2018-11-01 11:21:46 -0400

    Install nvm

--- scripts/dot.sh
+++ scripts/dot.sh
@@ -13,6 +13,16 @@ function init() {
   git submodule init
   git submodule update --recursive
 
+  # Install brew
+  if ! [ -x "$(command -v brew)" ]; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+
+  # Install nvm
+  if [ ! -d ~/.nvm ]; then
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+  fi
+
   # Setup homebrew
   brew tap homebrew/bundle
   brew bundle
