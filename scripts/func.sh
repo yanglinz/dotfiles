5fd21fdaa42c86fad6c3086eef37eade315d5d01
Yanglin Zhao <yz1876@gmail.com>
2018-12-13 15:50:43 -0600

    Don't wrap single var substitutions

--- scripts/func.sh
+++ scripts/func.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 function replace_and_backup() {
-  local source="${1}"
-  local target="${2}"
+  local source="$1"
+  local target="$2"
 
   cp -n "${target}" "${target}.bak"
   cp "${source}" "${target}"
