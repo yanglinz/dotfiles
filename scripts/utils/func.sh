53732e4f008774aa0a78168115c3cc1c167d3d4e
Yanglin Zhao <yz1876@gmail.com>
2019-01-12 23:37:12 -0500

    Tweak stow commands

--- scripts/utils/func.sh
+++ scripts/utils/func.sh
@@ -16,6 +16,13 @@ function is_personal_profile() {
   ./scripts/utils/profile.py | grep -q "profile:personal"
 }
 
+function slugify() {
+  declare -r str="$1"
+  
+  # Replace all / with -
+  echo "$str" | sed 's/\//-/g'
+}
+
 function replace_and_backup() {
   declare -r source="$1"
   declare -r target="$2"
