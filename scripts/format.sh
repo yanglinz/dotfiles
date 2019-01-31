3a96b2ecd3704e14b44bea55dbb567e74f4acfcd
Yanglin Zhao <yz1876@gmail.com>
2019-01-31 11:42:15 -0500

    Include manual targets for shell formatting

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
-git ls-files | grep "\.sh\$" | xargs shfmt -i 2 -w -s -ci -kp
+sh_targets=($(git ls-files | grep "\.sh\$"))
+
+sh_manual_targets=(
+  git-churn
+  git-cloc
+  git-lg
+  insecure-chrome
+  port-usage
+  bash_profile
+  bashrc
+)
+
+format_targets=("${sh_targets[@]}" "${sh_manual_targets[@]}")
+for f in "${format_targets[@]}"; do
+  git ls-files | grep "${f}\$" | xargs shfmt -i 2 -w -s -ci -kp
+done
