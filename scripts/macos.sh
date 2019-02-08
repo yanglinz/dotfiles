051a2e327a85eadaad26a54bbfa6a5546702c872
Yanglin Zhao <yz1876@gmail.com>
2019-02-08 13:42:57 -0500

    Only run macos.sh if it's osx

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -1,40 +1,48 @@
 #!/usr/bin/env bash
 
+source scripts/utils/func.sh
+
 # Taken most from https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 
-# Close any open System Preferences panes, to prevent them from overriding
-# settings we’re about to change
-osascript -e 'tell application "System Preferences" to quit'
+function setuo_osx() {
+  # Close any open System Preferences panes, to prevent them from overriding
+  # settings we’re about to change
+  osascript -e 'tell application "System Preferences" to quit'
 
-# Ask for the administrator password upfront
-sudo -v
+  # Ask for the administrator password upfront
+  sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
-# while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+  # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+  # while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Always show scrollbars
-defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
-# Possible values: `WhenScrolling`, `Automatic` and `Always`
+  # Always show scrollbars
+  defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
+  # Possible values: `WhenScrolling`, `Automatic` and `Always`
 
-# Disable the sound effects on boot
-sudo nvram SystemAudioVolume=" "
+  # Disable the sound effects on boot
+  sudo nvram SystemAudioVolume=" "
 
-# Restart automatically if the computer freezes
-sudo systemsetup -setrestartfreeze on
+  # Restart automatically if the computer freezes
+  sudo systemsetup -setrestartfreeze on
 
-# Set dock icon sizes
-defaults write com.apple.dock tilesize -int 36
-defaults write com.apple.dock magnification -bool true
-defaults write com.apple.dock largesize -int 64
+  # Set dock icon sizes
+  defaults write com.apple.dock tilesize -int 36
+  defaults write com.apple.dock magnification -bool true
+  defaults write com.apple.dock largesize -int 64
 
-# Kill affected applications
-affected_apps=(
-  "Address Book"
-  "Calendar"
-  "Contacts"
-  "Dock"
-  "Finder"
-)
-for app in "${affected_apps[@]}"; do
-  killall "${app}" &>/dev/null
-done
+  # Kill affected applications
+  affected_apps=(
+    "Address Book"
+    "Calendar"
+    "Contacts"
+    "Dock"
+    "Finder"
+  )
+  for app in "${affected_apps[@]}"; do
+    killall "${app}" &>/dev/null
+  done
+}
+
+if is_macos; then
+  macos
+fi
