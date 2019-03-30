bad4579ac5a7bef3c17b0f6bfbcd1f3cb9d78c9c
Yanglin Zhao <yz1876@gmail.com>
2019-03-29 23:07:17 -0400

    Sync root

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 function sync() {
-  rsync --exclude ".git/" \
-    --exclude ".DS_Store" \
-    -avh --no-perms ./sync/ "$HOME"
+  rsync --exclude ".DS_Store" \
+    -avh --no-perms ./sync-home/ "$HOME"
+
+  sudo rsync --exclude ".DS_Store" \
+    --chown=root:wheel \
+    --omit-dir-times \
+    -avh --no-perms ./sync-root/ /
 }
 
 if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
