72357b9a12d87b3460517718c06415ac139b7bb9
Yanglin Zhao <yz1876@gmail.com>
2021-08-20 21:31:43 -0400

    debug

--- .github/install-nix-darwin.sh
+++ .github/install-nix-darwin.sh
@@ -13,6 +13,10 @@ export NIX_PATH=darwin-config=$HOME/.nixpkgs/darwin-configuration.nix:$HOME/.nix
 git clone https://github.com/LnL7/nix-darwin.git ~/.nix-defexpr/darwin
 export NIX_PATH=darwin=$HOME/.nix-defexpr/darwin:darwin-config=$HOME/.nixpkgs/darwin-configuration.nix:$NIX_PATH
 
+echo "foo"
+ls -la ~/.nix-defexpr/darwin
+echo "bar"
+
 cp ~/.nix-defexpr/darwin/modules/examples/simple.nix ~/.nixpkgs/darwin-configuration.nix
 
 # you can also use this to rebootstrap nix-darwin in case
