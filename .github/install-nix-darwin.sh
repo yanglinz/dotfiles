3bf00bb02f52defc95a68175c8a74c2fb06d8f1f
Yanglin Zhao <yz1876@gmail.com>
2021-08-21 14:30:58 -0400

    New

--- .github/install-nix-darwin.sh
+++ .github/install-nix-darwin.sh
@@ -2,20 +2,25 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Or use a local git repository
+# Copy over default config
 git clone https://github.com/LnL7/nix-darwin.git ~/.nix-defexpr/darwin
 mkdir -p ~/.nixpkgs
 cp ~/.nix-defexpr/darwin/modules/examples/simple.nix ~/.nixpkgs/darwin-configuration.nix
 
-# Configure the channel
+echo "foo: 1"
+
+# Configure nix channel
 nix-channel --add https://github.com/LnL7/nix-darwin/archive/master.tar.gz darwin
 nix-channel --update
 
+echo "foo: 2"
+
+# Configure PATH
 export NIX_PATH=darwin-config=$HOME/.nixpkgs/darwin-configuration.nix:$HOME/.nix-defexpr/channels:$NIX_PATH
 export NIX_PATH=darwin=$HOME/.nix-defexpr/darwin:darwin-config=$HOME/.nixpkgs/darwin-configuration.nix:$NIX_PATH
 
-# you can also use this to rebootstrap nix-darwin in case
-# darwin-rebuild is to old to activate the system.
+echo "foo: 3"
+
 $(nix-build '<darwin>' -A system --no-out-link)/sw/bin/darwin-rebuild build
 $(nix-build '<darwin>' -A system --no-out-link)/sw/bin/darwin-rebuild switch
 
