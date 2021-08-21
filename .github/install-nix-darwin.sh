87b92bc7342d6f9f25002daffaaa3e5b4527ceb1
Yanglin Zhao <yz1876@gmail.com>
2021-08-20 21:24:49 -0400

    Try the manual installation script

---
+++ .github/install-nix-darwin.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+sudo ln -s private/var/run /run
+
+# Configure the channel
+nix-channel --add https://github.com/LnL7/nix-darwin/archive/master.tar.gz darwin
+nix-channel --update
+export NIX_PATH=darwin-config=$HOME/.nixpkgs/darwin-configuration.nix:$HOME/.nix-defexpr/channels:$NIX_PATH
+
+# Or use a local git repository
+git clone git@github.com:LnL7/nix-darwin.git ~/.nix-defexpr/darwin
+export NIX_PATH=darwin=$HOME/.nix-defexpr/darwin:darwin-config=$HOME/.nixpkgs/darwin-configuration.nix:$NIX_PATH
+
+cp ~/.nix-defexpr/darwin/modules/examples/simple.nix ~/.nixpkgs/darwin-configuration.nix
+
+# you can also use this to rebootstrap nix-darwin in case
+# darwin-rebuild is to old to activate the system.
+$(nix-build '<darwin>' -A system --no-out-link)/sw/bin/darwin-rebuild build
+$(nix-build '<darwin>' -A system --no-out-link)/sw/bin/darwin-rebuild switch
+
+. /etc/static/bashrc
