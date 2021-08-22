b61cc83d3249c4c7d2df84052e77edf9d3fc4956
Yanglin Zhao <yz1876@gmail.com>
2021-08-22 09:49:12 -0400

    Install nix darwin

--- .github/install-nix-darwin.sh
+++ .github/install-nix-darwin.sh
@@ -7,21 +7,16 @@ git clone https://github.com/LnL7/nix-darwin.git ~/.nix-defexpr/darwin
 mkdir -p ~/.nixpkgs
 cp ~/.nix-defexpr/darwin/modules/examples/simple.nix ~/.nixpkgs/darwin-configuration.nix
 
-echo "foo: 1"
-
 # Configure nix channel
 nix-channel --add https://github.com/LnL7/nix-darwin/archive/master.tar.gz darwin
 nix-channel --update
-
-echo "foo: 2"
 
 # Configure PATH
 export NIX_PATH=darwin-config=$HOME/.nixpkgs/darwin-configuration.nix:$HOME/.nix-defexpr/channels:$NIX_PATH
 export NIX_PATH=darwin=$HOME/.nix-defexpr/darwin:darwin-config=$HOME/.nixpkgs/darwin-configuration.nix:$NIX_PATH
 
-echo "foo: 3"
-
+# We can't get darwin-rebuild build/switch to work in CI _yet_
 # $(nix-build '<darwin>' -A system --no-out-link)/sw/bin/darwin-rebuild build
-$(nix-build '<darwin>' -A system --no-out-link)/sw/bin/darwin-rebuild switch
+# $(nix-build '<darwin>' -A system --no-out-link)/sw/bin/darwin-rebuild switch
 
-. /etc/static/bashrc
+# . /etc/static/bashrc
