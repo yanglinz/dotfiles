bd21b0c3ab9442f257308c8e60671a6c63f902af
Yanglin Zhao <yz1876@gmail.com>
2022-04-04 17:15:41 -0400

    Avoid outputting project local volta versions

--- macos/scripts/track-drift.sh
+++ macos/scripts/track-drift.sh
@@ -2,11 +2,15 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+DOTFILES_REPO_DIR=$(pwd)
+
 brew bundle dump --force && sort Brewfile -o Brewfile
-code --list-extensions | sort -f >./macos/drift/vscode-extensions.txt
-volta list --format=plain >./macos/drift/volta-list.txt
+code --list-extensions | sort -f > ./macos/drift/vscode-extensions.txt
+
+# We have to cd to a different direactory to avoid volta outputting repo local tool versions 
+(cd ~ && volta list > "${DOTFILES_REPO_DIR}/macos/drift/volta-list.txt")
 
 SPECTACLE_SHORTCUT="~/Library/Application Support/Spectacle/Shortcuts.json"
-[[ -f $SPECTACLE_SHORTCUT   ]] && cp "$SPECTACLE_SHORTCUT" ./macos/drift/spectacle.json
+[[ -f $SPECTACLE_SHORTCUT ]] && cp "$SPECTACLE_SHORTCUT" ./macos/drift/spectacle.json
 
 cp ~/.iterm/com.googlecode.iterm2.plist ./macos/home/iterm/
