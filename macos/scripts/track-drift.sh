04c39ef3bba0a077b555e72ff57aeccf950af5f5
Yanglin Zhao <yz1876@gmail.com>
2022-04-12 20:10:22 -0400

    Replace spectacle with rectangle

--- macos/scripts/track-drift.sh
+++ macos/scripts/track-drift.sh
@@ -10,7 +10,4 @@ code --list-extensions | sort -f > ./macos/drift/vscode-extensions.txt
 # We have to cd to a different direactory to avoid volta outputting repo local tool versions 
 (cd ~ && volta list > "${DOTFILES_REPO_DIR}/macos/drift/volta-list.txt")
 
-SPECTACLE_SHORTCUT="~/Library/Application Support/Spectacle/Shortcuts.json"
-[[ -f $SPECTACLE_SHORTCUT ]] && cp "$SPECTACLE_SHORTCUT" ./macos/drift/spectacle.json
-
 cp ~/.iterm/com.googlecode.iterm2.plist ./macos/home/iterm/
