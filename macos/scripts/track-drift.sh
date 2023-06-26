4a18790c44e2938e210ae7c1cd1fc32ffd897110
Yanglin Zhao <hi@yanglinzhao.com>
2023-06-25 20:31:09 -0400

    Remove references to kitty and iterm

--- macos/scripts/track-drift.sh
+++ macos/scripts/track-drift.sh
@@ -9,5 +9,3 @@ code --list-extensions | sort -f > ./macos/drift/vscode-extensions.txt
 
 # We have to cd to a different direactory to avoid volta outputting repo local tool versions 
 (cd ~ && volta list > "${DOTFILES_REPO_DIR}/macos/drift/volta-list.txt")
-
-cp ~/.iterm/com.googlecode.iterm2.plist ./macos/home/iterm/
