a546bfcd25046f969d46549128e1c67eb7a6ab6c
Yanglin Zhao <hi@yanglinzhao.com>
2023-11-02 20:00:57 -0400

    Run shell linting in CI with nix (#78)
    * Move nix flake to root of package
    
    * Try running command with nix develop
    
    * No with
    
    * Update
    
    * Try removing last modified
    
    * Try using version 17
    
    * Rename project
    
    * Fix shell linting errors

--- macos/home/shell/.bash_profile
+++ macos/home/shell/.bash_profile
@@ -1,4 +1,7 @@
+#!/usr/bin/env bash
+
 # Source bashrc if it exists
 if [ -f ~/.bashrc ]; then
+  # shellcheck disable=SC1090
   . ~/.bashrc
 fi
--- macos/scripts/track-drift.sh
+++ macos/scripts/track-drift.sh
@@ -2,7 +2,5 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-DOTFILES_REPO_DIR=$(pwd)
-
 brew bundle dump --force && sort Brewfile -o Brewfile
 code --list-extensions | sort -f >./macos/drift/vscode-extensions.txt
